a343bdc1ee0828d5320a0ca49507d6129f11bc48
oralxan <oralxan@users.noreply.github.com>
2024-01-28 21:19:52 +0500

    m

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-x@5kkn@g193--=ra7&&6e2@uj^=_+t*qur6g1=t^jfp!8@yl6c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ 'pinterest.pythonanywhere.com']
+ALLOWED_HOSTS = [ '*']
 
 
 # Application definition
@@ -114,6 +114,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
